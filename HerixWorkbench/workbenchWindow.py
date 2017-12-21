d63f2f0f357ce619e08bb090951a96f84ce49819
Phaulo Escalante <escalante.phaulo@outlook.com>
2017-12-21 14:43:20 -0600

    Added a little more.

---
+++ HerixWorkbench/workbenchWindow.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+"""
+Copyright (c) UChicago Argonne, LLC. All rights reserved.
+See LICENSE file.
+"""
+# ---------------------------------------------------------------------------------------------------------------------#
+from __future__ import unicode_literals
+
+import os
+import sys
+
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+
+
+
+class HerixWorkbenchWindow(QMainWindow):
+    """Main window class"""
+    def __init__(self, parent=None):
+        super(HerixWorkbenchWindow, self).__init__(parent)
+        self.setGeometry(50, 50, 1500, 800)
+        self.setWindowTitle("Herix Workbench")
+        self.setMinimumSize(1000, 650)
+
+def main():
+    """Main method.
+    """
+    app = QApplication(sys.argv)
+    herixWindow = HerixWorkbenchWindow()
+    herixWindow.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()

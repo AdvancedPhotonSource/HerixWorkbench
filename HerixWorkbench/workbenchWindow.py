c97ca083b84aa70cb33491d60e99a9df89416c04
Phaulo Escalante <escalante.phaulo@outlook.com>
2017-12-21 14:45:24 -0600

    This closes issue #1. Creates a menu bar with File => Open, which opens the spec file. Once the spec file is open it loads the scan into the ScanBrowser and the scan types to the ScanTypeSelector.

--- HerixWorkbench/workbenchWindow.py
+++ HerixWorkbench/workbenchWindow.py
@@ -12,6 +12,9 @@ import sys
 
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
+from spec2nexus.spec import SpecDataFile
+from specguiutils.scanbrowser import ScanBrowser
+from specguiutils.scantypeselector import ScanTypeSelector, SCAN_TYPES
 
 
 
@@ -22,6 +25,78 @@ class HerixWorkbenchWindow(QMainWindow):
         self.setGeometry(50, 50, 1500, 800)
         self.setWindowTitle("Herix Workbench")
         self.setMinimumSize(1000, 650)
+        self.windowSplitter = QSplitter()
+
+        self.CreateSpecDataSplitter()
+        self.createMenuBar()
+        self.windowSplitter.addWidget(self.specSplitter)
+        self.setCentralWidget(self.windowSplitter)
+
+    def CreateSpecDataSplitter(self):
+        self.specSplitter = QSplitter()
+        self.specSplitter.setOrientation(Qt.Vertical)
+        self.specSplitter.setFixedWidth(300)
+
+        self.scanBrowser = ScanBrowser()
+        self.scanTypeSelector = ScanTypeSelector()
+
+        self.specSplitter.addWidget(self.scanTypeSelector)
+        self.specSplitter.addWidget(self.scanBrowser)
+
+    def createMenuBar(self):
+        menuBar = self.menuBar()
+        menuBar.setNativeMenuBar(False)
+        fileMenu = menuBar.addMenu("File")
+
+        openAction = QAction("Open", self)
+        openAction.triggered.connect(self.openSpecFile)
+
+        exitAction = QAction("Exit", self)
+        exitAction.triggered.connect(self.close)
+
+        fileMenu.addAction(openAction)
+        fileMenu.addSeparator()
+        fileMenu.addAction(exitAction)
+
+    @pyqtSlot()
+    def openSpecFile(self):
+        fileName, specFilterName = QFileDialog.getOpenFileName(self, "Open spec file", None, "*.spec")
+        self.specFile = None
+
+        if fileName != "":
+            try:
+                self.specFile = SpecDataFile(fileName)
+                self.scans = self.specFile.scans
+                self.loadScans()
+            except Exception as ex:
+                QMessageBox.warning(self, "Loading error",
+                                    "There was an error loading the spec file. \n\nException: " + str(ex))
+
+    def loadScans(self):
+        self.scanBrowser.loadScans(self.scans)
+        scanTypes = self.getScanTypes()
+        self.scanTypeSelector.loadScans(scanTypes)
+        self.scanTypeSelector.scanTypeChanged.connect(self.filterScansByType)
+
+    def getScanTypes(self):
+        scanTypes = set()
+        for scan in self.scans:
+            scanTypes.add(self.specFile.scans[scan].scanCmd.split()[0])
+        scanTypes = list(scanTypes)
+        print(scanTypes)
+        scanTypes.sort(key=str.lower)
+        print(scanTypes)
+        return scanTypes
+
+    def filterScansByType(self):
+        if self.scanTypeSelector.getCurrentType() == 'All':
+            self.loadAllScans()
+        else:
+            self.scanBrowser.filterByScanTypes(self.scans, self.scanTypeSelector.getCurrentType())
+
+    def loadAllScans(self):
+        self.scanBrowser.loadScans(self.scans)
+
 
 def main():
     """Main method.

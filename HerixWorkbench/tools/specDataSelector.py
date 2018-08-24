788d3c4a049e6e67d414cecdbdcb2e9a29eb9901
Phaulo Escalante <escalante.phaulo@outlook.com>
2018-08-24 16:51:52 -0500

    This tabbed widget contains a tabbed for the spec counters and for the ana detectors, used for plotting.

--- HerixWorkbench/tools/specDataSelector.py
+++ HerixWorkbench/tools/specDataSelector.py
@@ -24,15 +24,32 @@ class SpecDataSelector(QTabWidget):
 
     # Signals
     detectorsSelected = pyqtSignal(list, name="detectorSelected")
+    countersSelected = pyqtSignal(list, name="countersSelected")
+    tabChanged = pyqtSignal(int, name="tabChanged")
 
     def __init__(self, parent=None):
         super(SpecDataSelector, self).__init__(parent)
         self.counterSelector = CounterSelector(counterOpts=COUNTER_OPTS)
+        self.counterSelector.counterView.counterDataChanged.connect(self.newCounterSelected)
         self.selectorContainer = SelectorContainer()
         self.selectorContainer.detectorsSelected.connect(self.anaDetectorSelected)
+        self.currentChanged.connect(self.newTabChanged)
 
         self.addTab(self.counterSelector, "Spec Data")
         self.addTab(self.selectorContainer, "Analyzers")
 
     def anaDetectorSelected(self, detectors):
         self.detectorsSelected[list].emit(detectors)
+
+    def loadCounters(self, scanLabels):
+        self.counterSelector.counterModel.initializeDataRows(COUNTER_OPTS, scanLabels)
+        self.counterSelector.counterModel.setCounterOptions(COUNTER_OPTS)
+        self.counterSelector.setSelectedCounters(["", ""])
+        self.newCounterSelected(self.counterSelector.getSelectedCounters())
+
+    def newCounterSelected(self, counters):
+        countersNames = self.counterSelector.getSelectedCounterNames(counters)
+        self.countersSelected[list].emit(countersNames)
+
+    def newTabChanged(self, index):
+        self.tabChanged[int].emit(index)
